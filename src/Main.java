c4dc6f2fbf9876ce5b4473325fa947b953051422
Asmaa Bengueddach <asmaa.bengueddach@gmail.com>
2016-11-04 20:58:27 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,15 @@ public class Main {
 			Point B =new Point(5);
 			Point C =new Point(3,8);
 			
-			System.out.println("* Créer 3 Points dans l'espace on utilisant 3 consctructeur différents ");
-			System.out.println("-Le Point A mes coordonnées sont : A"+A.toString());
-			System.out.println("-Le Point B mes coordonnées sont : B"+B.toString());
-			System.out.println("-Le Point C mes coordonnées sont : C"+C.toString());
+			System.out.println("* CrÃ©er 3 Points dans l'espace on utilisant 3 consctructeur diffÃ©rents ");
+			System.out.println("-Le Point A mes coordonnÃ©es sont : A"+A);
+			System.out.println("-Le Point B mes coordonnÃ©es sont : B"+B);
+			System.out.println("-Le Point C mes coordonnÃ©es sont : C"+C);
 			
 			
 			System.out.println("* Donner les images de A , B et C ");
-			System.out.println("-L'image de A est : A-1"+A.imagePoint());
-			System.out.println("-L'image de B est : B-1"+B.imagePoint());
-			System.out.println("-L'image de C est : C-1"+C.imagePoint());
+			System.out.println("-L'image de A est : A-1"+A);
+			System.out.println("-L'image de B est : B-1"+B);
+			System.out.println("-L'image de C est : C-1"+C);
 		}
 }
